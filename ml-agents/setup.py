28a74d4b71706ed209ae3b57f4ef8a89f930084e
5eqn <subcat2077@gmail.com>
2025-12-31 10:55:51 +0800

    Add dependencies

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -57,6 +57,8 @@ setup(
     zip_safe=False,
     install_requires=[
         # Test-only dependencies should go in test_requirements.txt, not here.
+        "six>=1.17.0",
+        "onnx>=1.17.0",
         "grpcio>=1.11.0",
         "h5py>=2.9.0",
         f"mlagents_envs=={VERSION}",
